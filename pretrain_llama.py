17faa582543275a4882eb4b552e9051922d882c8
wen <zwen_020@163.com>
2024-06-03 20:38:42 +0800

    add llama

--- pretrain_llama.py
+++ pretrain_llama.py
@@ -193,7 +193,8 @@ def train_valid_test_datasets_provider(train_val_test_num_samples):
         dataset_type = GPTDataset
 
     print_rank_0("> building train, validation, and test datasets for GPT ...")
-
+    # TODO test setting the number of samples
+    train_val_test_num_samples = [30, 10, 2]
     train_ds, valid_ds, test_ds = BlendedMegatronDatasetBuilder(
         dataset_type,
         train_val_test_num_samples,
