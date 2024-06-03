19d8f9fc31d876c6e12fe6632210ec32bbcda79e
wen <zwen_020@163.com>
2024-06-03 16:06:36 +0800

    add llama

--- megatron/legacy/model/__init__.py
+++ megatron/legacy/model/__init__.py
@@ -5,6 +5,7 @@ from .rms_norm import RMSNorm
 
 from .bert_model import BertModel
 from .gpt_model import GPTModel
+from .llama_model import LLaMAModel
 from .t5_model import T5Model
 from .language_model import get_language_model
 from .module import Float16Module
