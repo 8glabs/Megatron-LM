4fc99363b8b55b57e10e0dab04cabe8fc1c16327
wen <zwen_020@163.com>
2024-06-04 10:03:50 +0800

    add llama

--- examples/pretrain_llama2_distributed.sh
+++ examples/pretrain_llama2_distributed.sh
@@ -36,8 +36,8 @@ LLAMA2_ARGS="
     --num-layers 32 \
     --hidden-size 4096 \
     --num-attention-heads 32 \
-    --seq-length 1024 \
-    --max-position-embeddings 1024 \
+    --seq-length 512 \
+    --max-position-embeddings 512 \
     --micro-batch-size 1 \
     --global-batch-size 1 \
     --lr 0.00015 \
